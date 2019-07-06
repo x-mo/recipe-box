ca9f7e69388d4c65b2ad7569e3370db0aa1c5a72
Karim Khalifa <karimkhalifa@hotmail.com>
2019-07-06 15:09:08 +0200

    added bower for jquery and bootstrap

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
+const path = require("path");
 const express = require('express');
-var usersRouter = require('./routes/usersRouter');
-var recipesRouter = require('./routes/recipesRouter');
-var mainRouter = require('./routes/mainRouter');
-var ingrediantsRouter = require('./routes/ingrediantsRouter');
-var bodyParser = require('body-parser');
+const usersRouter = require('./routes/usersRouter');
+const recipesRouter = require('./routes/recipesRouter');
+const mainRouter = require('./routes/mainRouter');
+const ingrediantsRouter = require('./routes/ingrediantsRouter');
+const bodyParser = require('body-parser');
 
 const app = express();
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
+app.use(express.static(path.join(__dirname, 'bower_components')));
 app.use(bodyParser.urlencoded());
 app.use(express.json());
 app.use('/', mainRouter);
