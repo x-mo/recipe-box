c720f822c4e63b7590eec1e890e6de19422cee93
X Mohamed Ahmed <X_MohamedAhmed@yahoo.com>
2019-07-07 08:49:39 +0200

    Implemented temp Header & Footer

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,28 +1,39 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header/>
+    <Footer/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue'
+import Footer from './components/Footer.vue'
+
+import axios from 'axios';
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Header,
+    Footer
+  },
+//   data() {
+//     // return {
+//     //   todos: []
+//     // }
+//   },
+  created() {
+    axios.get('http://localhost:8080/ingrediants/')
+      .then(res => {console.log(res.data);})
+      .catch(() => { console.error("Fetching error occured.");} );
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+* {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
 </style>
---
+++ frontend/src/components/Footer.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="footer">
+      <footer class="footer has-background-success">
+        
+        <div class="socialmedia">
+          <div class="w-100 white sans-serif flex flex-column justify-center items-center">
+            <div class="mw8 center overflow-hidden">
+              <ul class="list ma0 pa0 flex flex-row flex-wrap gray8 has-background-success">
+                <li class="pv3 ph2 ma0 link grow">
+                  <a class="gray8" target="_blank" href="https://www.youtube.com/">
+                    <font-awesome-icon :icon="['fab', 'youtube']" size="2x" />
+                  </a>
+                </li>
+                <li class="pv3 ph2 ma0 link grow">
+                  <a class="gray8" target="_blank" href="https://www.facebook.com/">
+                    <font-awesome-icon :icon="['fab', 'facebook']" size="2x" />
+                  </a>
+                </li>
+                <li class="pv3 ph2 ma0 link grow">
+                  <a class="gray8" target="_blank" href="https://www.twitter.com/">
+                    <font-awesome-icon :icon="['fab', 'twitter']" size="2x" />
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+          
+        </div>
+        <!-- SOCIALMEDIA END -->
+        <div class="content has-text-centered">
+          <p>
+            <strong>RecipeBox</strong>
+            © 2019 All Rights Reserved.
+          </p>
+        </div>
+        <!-- COPYRIGHTS END -->
+      </footer>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Footer',
+}
+</script>
+
+<style scoped>
+
+</style>
