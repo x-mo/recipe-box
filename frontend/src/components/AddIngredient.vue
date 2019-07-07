49ec30590dd15c2426cd1732b6e0332450ef46cb
X Mohamed Ahmed <X_MohamedAhmed@yahoo.com>
2019-07-08 01:49:55 +0200

    Fetching ingredients & load instantaneously.. still ugly.

--- frontend/src/components/AddIngredient.vue
+++ frontend/src/components/AddIngredient.vue
@@ -2,15 +2,18 @@
 <div class="addingredient">
     <h1>Adding Ingredient Component</h1>
     <form @submit.prevent="addIngredient">
-      <input type="text" v-model="name" name="name" placeholder="Ingredient">
-      <input type="text" v-model="weight" name="weight" placeholder="Weight">
-      <input type="text" v-model="price" name="price" placeholder="Price">
+      <input type="text" v-model="name" name="name" placeholder="Ingredient" v-validate="'required'">
+      <p class="alert" v-if="errors.has('name')">{{errors.first('name')}}</p>
+      <input type="number" step="any" v-model="weight" name="weight" placeholder="Weight" v-validate="'required'">
+      <p class="alert" v-if="errors.has('weight')">{{errors.first('weight')}}</p>
+      <input type="number" step="any" v-model="price" name="price" placeholder="Price" v-validate="'required'">
+      <p class="alert" v-if="errors.has('price')">{{errors.first('price')}}</p>
       <input type="submit" value="Submit" class="btn">
     </form>
 
     <ul>
         <transition-group name="list" enter-active-class="animated bounceInUp" leave-active-class="animated bounceOutDown">
-          <li v-for="(data, index) in ingredientsArray" :key="index+1">
+          <li v-for="(data, index) in ingredientsArray" :key="index">
             {{ data.name }} {{ data.weight }} {{ data.price }}
             <i class="fa fa-minus-circle" v-on:click="remove(index)"></i>
           </li>
@@ -40,8 +43,8 @@ export default {
       console.log(newIngredient);
 
     axios.post('http://localhost:8080/ingrediants/add/',newIngredient)
-      .then(res => {console.log(res.data);})
-      .catch(err => { console.log(err);} );
+      .then(res => {this.ingredientsArray.push(newIngredient); console.log(this.ingredientsArray);})
+      .catch(err => { alert("Fill Required Fields"); console.log(err);} );
 
     }
   },
