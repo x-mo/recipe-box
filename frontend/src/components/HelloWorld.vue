ffc337577a0d5bb7c78dec14dbe44c6853cb2de4
X Mohamed Ahmed <X_MohamedAhmed@yahoo.com>
2019-07-07 06:41:16 +0200

    Added blank Vue

---
+++ frontend/src/components/HelloWorld.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="container">
+    Still
+  </div>
+</template>
+
+<script>
+
+import axios from 'axios';
+
+
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+//   data() {
+//     // return {
+//     //   todos: []
+//     // }
+//   },
+  created() {
+    axios.get('../ingrediants/')
+      .then(res => console.log(res.data))
+      .catch(err => console.log(err));
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
