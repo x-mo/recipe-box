ccad27b74bc0929da4a66ffac895833c5dca621b
X Mohamed Ahmed <X_MohamedAhmed@yahoo.com>
2019-07-10 05:02:57 +0200

    Push recipe

--- frontend/src/components/AddRecipe.vue
+++ frontend/src/components/AddRecipe.vue
@@ -68,13 +68,17 @@
         enter-active-class="animated bounceInUp"
         leave-active-class="animated bounceOutDown"
       >
-        <li v-for="(data, index) in ingredientsArray" :key="index">
-          {{ data.ingName }} {{ data.ingWeight }} {{ data.ingPrice }}
+        <li v-for="(data, index) in recipesArray" :key="index">
+          {{ data.recipeName }} {{ data.recipePrice }}
           <i
             class="fa fa-minus-circle"
             v-on:click="remove(index)"
           ></i>
+          <!-- <li v-for="(data,iindex) in data.recipeIngr" :key="iindex"> {{ data.ingName }}</li> -->
+        
         </li>
+<!-- <li v-for="(data,index) in ingredientsArray" :key="index"> {{ data.ingName }}</li> -->
+        
       </transition-group>
     </ul>
   </div>
@@ -87,23 +91,41 @@ export default {
   name: "AddRecipe",
   data() {
     return {
-      ingredientsArray: []
+      ingredientsArray:[{}],
+      recipesArray: []
     };
   },
   methods: {
     addRecipe() {
-      const newIngredient = {
-        ingName: this.ingName,
-        ingWeight: this.ingWeight,
-        ingPrice: this.ingPrice
+      console.log("addRecipe(): Called.");
+      const newRecipe = {
+        // ingName: this.ingName,
+        // recipeIngr: this.recipeIngr,
+        // ingWeight: this.ingWeight,
+        // ingPrice: this.ingPrice
+        recipeName: "recNAME",
+        recipeIngr: [{
+          ingName:"ingNAME1",
+          ingWeight:1,
+          ingPrice:5
+        },{
+          ingName:"ingNAME2",
+          ingWeight:1,
+          ingPrice:5
+        },{
+          ingName:"ingNAME3",
+          ingWeight:1,
+          ingPrice:5
+        }],
+        recipePrice: 10
       };
-      console.log(newIngredient);
+      //console.log(newIngredient);
 
       axios
-        .post("http://localhost:8080/recipes/add/", newIngredient)
+        .post("http://localhost:8080/recipes/add/", newRecipe)
         .then(() => {
-          this.ingredientsArray.push(newIngredient);
-          console.log(this.ingredientsArray);
+          this.recipesArray.push(newRecipe);
+          console.log(this.recipesArray);
         })
         .catch(err => {
           alert("Fill Required Fields");
@@ -112,11 +134,17 @@ export default {
     }
   },
   created() {
+    this.addRecipe();
     axios
-      .get("http://localhost:8080/ingredients/api/")
+      .get("http://localhost:8080/recipes/api/")
       .then(res => {
-        console.log(res.data);
-        this.ingredientsArray = res.data;
+        //console.log(res.data);
+        this.recipesArray = res.data;
+        this.ingredientsArray = recipesArray.recipeIngr;
+        // console.log(this.ingredientsArray);
+        
+        console.log(res.data.recipeIngr);
+        console.log(this.ingredientsArray);
       })
       .catch(err => console.log(err));
   }
