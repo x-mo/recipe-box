f987b0d89886d86630f71c14fba2b59d05c752f8
X Mohamed Ahmed <X_MohamedAhmed@yahoo.com>
2019-07-11 01:30:33 +0200

    Dropdown list reads from API

--- frontend/src/components/MultiSelectComp.vue
+++ frontend/src/components/MultiSelectComp.vue
@@ -1,48 +1,63 @@
 <template>
   <div>
-    <multiselect v-model="selected" multiple=true :options="options"></multiselect>
+    <multiselect
+      v-model="selected"
+      multiple="true"
+      :options="options"
+      :hide-selected="true"
+      :taggable="true"
+      :custom-label="ingrFormat"
+    ></multiselect>
+    {{selected}}
+    <br />
     <button @click="logit">Log the fuck up</button>
   </div>
 </template>
 
 <script>
-import axios from 'axios';
 import Multiselect from "vue-multiselect";
+import axios from "axios";
 export default {
   components: { Multiselect },
   data() {
     return {
       selected: null,
-      // options: ["list", "of", "options"]
-      options: [{
-        "ingName": "Maccaronie",
-        "ingWeight": 1,
-        "ingPrice": 18
-    },{
-        "ingName": "Maccaronie",
-        "ingWeight": 1,
-        "ingPrice": 18
-    },{
-        "ingName": "Maccaronie",
-        "ingWeight": 1,
-        "ingPrice": 18
-    }]
+      value: { ingName: "Tomatoes", ingWeight: 1, ingPrice: 10 },
+      options: [
+        {
+          ingName: "Tomatoes",
+          ingWeight: 1,
+          ingPrice: 10
+        },
+        {
+          ingName: "Cheese",
+          ingWeight: 1,
+          ingPrice: 20
+        },
+        {
+          ingName: "Cucumber",
+          ingWeight: 1,
+          ingPrice: 30
+        }
+      ]
     };
-  },
-  methods: {
-    logit: function() {
-      console.log(this.selected);
-    }
   },
   created() {
     axios
       .get("http://localhost:8080/ingredients/api/")
       .then(res => {
-        console.log(res.data.ingName);
-        // this.options = res.data;
-        // this.options = this.options.ingName
+        console.log(res.data);
+        this.options = res.data;
       })
       .catch(err => console.log(err));
+  },
+  methods: {
+    logit: function() {
+      console.log(this.selected);
+    },
+    ingrFormat: function({ingName, ingWeight, ingPrice}) {
+      return `${ingName} — ${ingWeight} — ${ingPrice}`;
+    }
   }
 };
 </script>
