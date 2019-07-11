7ffa17217b3d91cd0cbf3ff87ec05bab1c3d0af7
Karim Khalifa <karimkhalifa@hotmail.com>
2019-07-11 22:57:11 +0200

    _id

--- models/ingredientModel.js
+++ models/ingredientModel.js
@@ -3,12 +3,14 @@ const mongoose  = require('mongoose');
 
 
 const ingredientSchema = new mongoose.Schema({
+    _id: String,
     ingName: String,
     ingWeight: Number,
     ingPrice: Number
 });
 
 const ingredient  = {
+    _id: Joi.string().min(3).max(30).required(),
     ingName: Joi.string().min(3).max(30).required(),
     ingWeight: Joi.number().precision(2).required(),
     ingPrice: Joi.number().required()
