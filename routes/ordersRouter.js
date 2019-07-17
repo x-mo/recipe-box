e907309dc13258547bf4650ed674e328f1e40b60
Karim Khalifa <karimkhalifa@hotmail.com>
2019-07-17 17:36:04 +0200

    added order status and routes

--- routes/ordersRouter.js
+++ routes/ordersRouter.js
@@ -27,6 +27,12 @@ orderRouter.get('/api/findOpenOrders',(req,res) => {
   });
 });
 
+orderRouter.get('/api/findPendingOrders',(req,res) => {
+  OrderModel.OrderModel.find({orderStatus : "Pending"}, (err, orders) => {
+    res.send(orders);
+  });
+});
+
 orderRouter.get('/api/findClosedOrders',(req,res) => {
   OrderModel.OrderModel.find({orderStatus : "Done"}, (err, orders) => {
     res.send(orders);
@@ -43,7 +49,7 @@ orderRouter.post('/api/add',(req,res,next) => {
     const newOrder = new OrderModel.OrderModel(req.body);
     newOrder.save()
     .then(item => {
-      res..status(200).send("order saved to database");
+      res.status(200).send("order saved to database");
     })
     .catch(err => {
       res.status(400).send(err.errmsg);
